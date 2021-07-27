20ab2476623e5b9333ab1e7e746a8f3ecf22530d
michael jones <michael.jowen.jones@gmail.com>
2021-07-27 14:39:44 +0100

    isEqual challenge

---
+++ challenges/check-if-equal.js
@@ -0,0 +1,14 @@
+`use strict`;
+function isEqual(a,b){
+    if (a === b){
+        return true
+    }
+    return false
+}
+
+console.log('isEqual(4,4):',isEqual(4, 4), 'should be True')
+console.log('isEqual(0,0):',isEqual(0, 0), 'should be True')
+console.log('isEqual(-4,-4):',isEqual(-4, -4), 'should be True')
+console.log('isEqual(4,-4):',isEqual(4, -4), 'should be False')
+console.log('isEqual(0,1):',isEqual(0, 1), 'should be False')
+
