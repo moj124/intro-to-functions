17c52573d82bf7f4288129e8a4961bdaafd782fd
michael jones <michael.jowen.jones@gmail.com>
2021-07-27 15:49:57 +0100

    added longest-word-in-list challenge

---
+++ challenges/longest-word-in-list.js
@@ -0,0 +1,14 @@
+function longestInList(arr){
+    let longest = arr[0]
+
+    for (word of arr){
+        if (word.length > longest.length){
+            longest = word
+        }
+    }
+    return longest
+}
+
+console.log('longestInList(["England", "Wales", "Scotland: ", "Northern Ireland"])', longestInList(["England", "Wales", "Scotland", "Northern Ireland"]), 'should be "Northern Ireland"')
+console.log('longestInList(["the", "quick", "brown", "fox"]): ', longestInList(["the", "quick", "brown", "fox"]), 'should be "quick"')
+console.log('longestInList(["hello", "hi", "greetings", "hey"]): ', longestInList(["hello", "hi", "greetings", "hey"]), 'should be "greetings"')
